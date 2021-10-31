c5b4e0a5190b0fec9924c858266d80780b5b9c7b
creatoff <creatoff@gmail.com>
2021-10-31 15:10:53 +0400

    remove silent flags from tests script

--- runTests.js
+++ runTests.js
@@ -3,7 +3,7 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-exec('npm test --silent -- --silent').then(({ stdout, stderr }) => {
+exec('npm test').then(({ stdout, stderr }) => {
   if (stderr) {
     console.error(stderr);
   }
